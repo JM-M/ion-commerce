94ba74879051794a77048d720310b2f849b782d2
Jola-Moses Michael <jolamoses@gmail.com>
2023-11-13 23:21:08 +0100

    Added github link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,14 @@ type Props = {};
 const Footer = (props: Props) => {
   return (
     <footer className='py-16 mt-10 bg-gray-200'>
-      <div className='container !px-0 text-center text-gray-700'>
-        Ion Ecommerce &copy; {new Date().getFullYear()}
+      <div className='container !px-0 text-center text-gray-900'>
+        Ion Ecommerce &copy; {new Date().getFullYear()}{' '}
+        <a
+          href='https://github.com/JM-M'
+          className='font-medium text-blue-700 hover:underline'
+        >
+          Michael Jola-Moses
+        </a>
       </div>
     </footer>
   );
